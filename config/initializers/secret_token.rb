d10d21161a7bc7be829faefd9ef8636f69b60171
Gavilan Galloway <gavilan@ubuntu.(none)>
2013-03-16 16:48:39 -0700

    New starter implementation without ActiveRecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pwrwebsite::Application.config.secret_token = 'd440ad495ccbbcc2afc9f11452f9f7c0ce4aa978ab61d92ecee6864163130694bfc8a7c2bcac323e0df8a78e91097b238bfc12988c91bc115bae307a041003d8'
